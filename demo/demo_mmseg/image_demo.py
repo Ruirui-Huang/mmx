f581283ba511fe25908732c90fd5fd9604bff149
Ruirui-Huang <2440944673@qq.com>
2024-03-26 23:54:55 +0800

    <fix> 代码重构

--- demo/demo_mmseg/image_demo.py
+++ demo/demo_mmseg/image_demo.py
@@ -16,37 +16,41 @@ from mmseg.apis import inference_model
 sys.path.append(osp.dirname(osp.dirname(osp.dirname(osp.realpath(__file__)))))
 from mmx.apis import init_model
 warnings.filterwarnings("ignore")
+import torch.multiprocessing as mp
 
 def parse_args():
     parser = ArgumentParser()
     parser.add_argument(
         'imgs', 
-        help='Image file')
+        help='Path to the image files')
     parser.add_argument(
         'config', 
-        help='Config file')
+        help='Path to the configuration file for the model')
     parser.add_argument(
         'checkpoint', 
-        help='Checkpoint file')
+        help='Path to the checkpoint file of the model.')
     parser.add_argument(
         'out', 
-        help='Path to output file')
+        help='Path to the output directory where processed images will be saved')
     parser.add_argument(
         '--GT', 
-        help='Path to label file', 
-        default=None)
+        default=None,
+        help='Optional path to the ground truth label file')
     parser.add_argument(
         '--roi',
         type=ast.literal_eval, 
-        default=None,)
+        default=None,
+        help='Optional region of interest in the image as a tuple (x, y, x, y')
     parser.add_argument(
         '--weights',
         type=ast.literal_eval,
-        default=None)
+        default=None,
+        help='Optional weights for different classes in the form of a list')
     parser.add_argument(
         '--num-crop',
         type=int, 
-        default=4)
+        default=4,
+        help='Number of crops to be created from the image')
     parser.add_argument(
         '--device', 
         default='cuda:0', 
@@ -59,11 +63,13 @@ def parse_args():
     parser.add_argument(
         '--probs', 
         type=ast.literal_eval, 
-        default=[0., 1.])
+        default=[0., 1.],
+        help='List of probabilities for data augmentation')
     parser.add_argument(
         '--directions', 
         type=ast.literal_eval, 
-        default=['horizontal'])
+        default=['horizontal'],
+        help='List of directions for data augmentation')
     parser.add_argument(
         '--thr',
         type=float,
@@ -76,14 +82,7 @@ def parse_args():
     return args
 
 # 结果可视化
-def imshow_semantic(img,
-                    seg,
-                    mask,
-                    palette,
-                    save_path=None,
-                    opacity=0.5,
-                    rect=[]):
-
+def imshow_semantic(img, seg, mask, palette, save_path=None, opacity=0.5, rect=[]):
     mkdir_or_exist(osp.dirname(save_path))
     if isinstance(img, str): img = mmcv.imread(img)
     H, W = seg.shape
@@ -110,6 +109,30 @@ def imshow_semantic(img,
 
     mmcv.imwrite(drawn_img, save_path)
 
+# 计算各个切片区域的置信度矩阵
+def process_slice_Acc(i, j, h, w, gt, result_seg_pred, num_classes, weights):
+    gt_crop = gt[0, h[i]:h[i+1], w[j]:w[j+1]]
+    result_crop = result_seg_pred[0, h[i]:h[i+1], w[j]:w[j+1]]
+    inds = (num_classes * gt_crop + result_crop).flatten()
+    mat = torch.bincount(inds, minlength=num_classes**2).view(num_classes, num_classes).float()
+    per_label_sums = mat.sum(axis=1)
+    idx = (per_label_sums > 0) & torch.tensor(weights, dtype=torch.bool)
+    per_label_sums[per_label_sums==0] = 1 
+    mat /= per_label_sums[:, None]
+    avg_acc = mat.diag()[idx].mean()
+    return avg_acc.item(), w[j], h[i], w[j+1], h[i+1]
+
+def process_slice_IoU(i, j, h, w, gt, result_seg_pred, num_classes, weights):
+    gt_crop = gt[0, h[i]:h[i+1], w[j]:w[j+1]]
+    result_crop = result_seg_pred[0, h[i]:h[i+1], w[j]:w[j+1]]
+    inds = (num_classes * gt_crop + result_crop).flatten()
+    mat = torch.bincount(inds, minlength=num_classes**2).view(num_classes, num_classes).float()
+    intersection = torch.diag(mat)
+    union = mat.sum(axis=1) + mat.sum(axis=0) - intersection
+    idx = (union > 0) & torch.tensor(weights, dtype=torch.bool)
+    iou = (intersection / union)[idx].mean()
+    return iou.item(), w[j], h[i], w[j+1], h[i+1]
+
 # 一致性校验流程：校验同一输入图像信号经过不同强度的扰动后，预测结果的期望是否一致
 def consistency_checking(img, mask, save_path, name, results, palette, thr=0.5, opacity=0.5, weights=[], num_crop=3):
     num_classes = len(palette)
@@ -135,23 +158,13 @@ def consistency_checking(img, mask, save_path, name, results, palette, thr=0.5, 
         result_seg_pred = (result_seg_logits > 0.5).to(result_seg_logits)
     # 为了减少耗时，无法进行像素级校验，因此只能计算各类别像素整体的置信度是否一致，校验精度较差
     # 为了提高校验精度，将图片裁剪为多个切片，再逐一校验
-    conf = np.empty((num_crop, num_crop, 5))
-    h = np.linspace(0, H, num_crop+1, dtype=int)
-    w = np.linspace(0, W, num_crop+1, dtype=int)
-    for i in range(num_crop):
-        for j in range(num_crop):
-            # 计算各个切片区域的置信度矩阵    
-            gt_crop = gt[0, h[i]:h[i+1], w[j]:w[j+1]]
-            result_crop = result_seg_pred[0, h[i]:h[i+1], w[j]:w[j+1]]
-            inds = num_classes * gt_crop + result_crop
-            inds = inds.flatten()
-            mat = np.bincount(inds, minlength=num_classes**2).reshape(num_classes, num_classes)
-            per_label_sums = mat.sum(axis=1)[:, np.newaxis]
-            idx = (per_label_sums > 0).flatten()
-            idx = [(idx[w] and bool(weights[w])) for w in range(num_classes)]
-            per_label_sums = np.where(per_label_sums==0, 1, per_label_sums) 
-            mat = mat.astype(np.float32) / per_label_sums
-            conf[i][j] = [np.diagonal(mat)[idx].mean(), w[j], h[i], w[j+1], h[i+1]]
+    h = np.linspace(0, H, num_crop+1, dtype=torch.int)
+    w = np.linspace(0, W, num_crop+1, dtype=torch.int)
+    with mp.Pool(mp.cpu_count()) as pool:
+        conf = pool.starmap(process_slice_Acc, [(i, j, h, w, gt, result_seg_pred, num_classes, weights) for i in range(num_crop) for j in range(num_crop)])
+    
+    conf = torch.tensor(conf)
+    conf = conf.view(num_crop, num_crop, 5)        
 
     low_conf_idx = conf[:,:,0] < thr
     if low_conf_idx.any(): floder = 'inconsistent'
@@ -165,6 +178,30 @@ def consistency_checking(img, mask, save_path, name, results, palette, thr=0.5, 
         save_path=osp.join(osp.join(save_path, floder), name),
         opacity=opacity,
         rect=conf[low_conf_idx][:, 1:])
+
+def get_data(img_path, gt, roi):
+    image = mmcv.imread(img_path)
+    # 支持有/无Gt
+    if gt: 
+        img_bytes = fileio.get(osp.join(gt, osp.split(img_path)[-1].replace('.jpg', '.png')))
+        mask = mmcv.imfrombytes(img_bytes, flag='unchanged').squeeze().astype(np.uint8)
+    else: mask = None
+
+    # 支持设定规则区域
+    if isinstance(roi, tuple):
+        crop_x1, crop_y1, crop_x2, crop_y2 = roi
+        image = image[crop_y1:crop_y2, crop_x1:crop_x2]
+        if not mask: mask = mask[crop_y1:crop_y2, crop_x1:crop_x2]
+    elif isinstance(roi, dict):
+        key = '_'.join(osp.split(img_path)[-1].split('_')[:-1])
+        try:
+            crop_x1, crop_y1, crop_x2, crop_y2 = roi[key]
+            image = image[crop_y1:crop_y2, crop_x1:crop_x2]
+            if not mask: mask = mask[crop_y1:crop_y2, crop_x1:crop_x2]
+        except: pass
+    elif roi == None: pass
+    else: print("ROI设置有误！")
+    return image, mask
 
 def inference_detector_custom(args, img, img_scale, prob, direction):
     
@@ -211,50 +248,23 @@ def main():
     img_scale = cfg.img_scale
     palette = cfg.metainfo.palette
     palette = np.array(palette)
-    root = args.imgs
     imglist = list(scandir(args.imgs, ('.jpg', '.png'), recursive=True))
     
     ctx = torch.multiprocessing.get_context("spawn")
     num_tasks = len(imglist)
-    nproc = args.nproc if num_tasks > args.nproc else num_tasks
-    pool = ctx.Pool(args.nproc)
+    pool = ctx.Pool(args.nproc if num_tasks > args.nproc else num_tasks)
 
     prog_bar = ProgressBar(num_tasks, num_tasks, True, file=sys.stdout)
     for img in imglist:
         img_path = osp.join(args.imgs, img)
-        image = mmcv.imread(img_path)
-        # 支持有/无Gt
-        if args.GT: 
-            img_bytes = fileio.get(osp.join(args.GT, osp.split(img_path)[-1].replace('.jpg', '.png')))
-            mask = mmcv.imfrombytes(img_bytes, flag='unchanged').squeeze().astype(np.uint8)
-        else: mask = None
+        image, mask = get_data(img_path, args.GT, args.roi)
 
-        # 支持设定规则区域
-        if isinstance(args.roi, tuple):
-            crop_x1, crop_y1, crop_x2, crop_y2 = args.roi
-            image = image[crop_y1:crop_y2, crop_x1:crop_x2]
-            if not mask: mask = mask[crop_y1:crop_y2, crop_x1:crop_x2]
-        elif isinstance(args.roi, dict):
-            key = '_'.join(osp.split(img_path)[-1].split('_')[:-1])
-            try:
-                crop_x1, crop_y1, crop_x2, crop_y2 = args.roi[key]
-                image = image[crop_y1:crop_y2, crop_x1:crop_x2]
-                if not mask: mask = mask[crop_y1:crop_y2, crop_x1:crop_x2]
-            except: pass
-        elif args.roi == None: pass
-        else: print("ROI设置有误！")
-        
         results = []
         for prob in args.probs:
             for direction in args.directions:
                 result = pool.apply_async(
                     inference_detector_custom, 
-                    args=(
-                        args, 
-                        image,
-                        img_scale,
-                        prob, 
-                        direction))
+                    args=(args, image, img_scale, prob, direction))
                 results.append(result)
 
         # 一致性校验
